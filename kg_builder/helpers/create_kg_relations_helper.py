b32b07c826773cb742260d6f069a993a94771861
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-03-29 22:05:24 +0530

    adding helper to insert nodes into knowledge graph and make relations

---
+++ kg_builder/helpers/create_kg_relations_helper.py
@@ -0,0 +1,52 @@
+import json
+
+from neo4j import GraphDatabase, Result
+
+uri = "bolt://localhost:7687"
+username = "neo4j"
+password = "neo4j1234!"
+database_name = "neo4j"
+
+driver = GraphDatabase.driver(uri, database=database_name, auth=(username, password))
+
+
+def create_entity_node(tx, entity_text, label):
+    query = """
+    MERGE (e:Entity {text: $entity_text, label: $label})
+    RETURN e
+    """
+    result = tx.run(query, entity_text=entity_text, label=label)
+    return result.single()[0]
+
+
+def create_relationship(tx, entity1_data, entity2_data, relation):
+    query = """
+    MERGE (e1:Entity {text: $entity1_text, label: $entity1_label})
+    MERGE (e2:Entity {text: $entity2_text, label: $entity2_label})
+    MERGE (e1)-[r:RELATION {name: $relation}]->(e2)
+    RETURN r
+    """
+    tx.run(
+        query,
+        entity1_text=entity1_data["entity_text"],
+        entity1_label=entity1_data["label"],
+        entity2_text=entity2_data["entity_text"],
+        entity2_label=entity2_data["label"],
+        relation=relation
+    )
+
+def insert_relationships(tx, relationships_list):
+    for relationship_data in relationships_list:
+        create_relationship(tx, relationship_data["entity1"], relationship_data["entity2"],
+                            relationship_data["relation"])
+
+
+def create_and_insert_relations_into_kg(json_file_path):
+    with open(json_file_path, 'r') as f:
+        json_data = f.read()
+    data = json.loads(json_data)
+    with driver.session() as session:
+        for relationships in data:
+            session.execute_write(insert_relationships, relationships)
+    print("Relationships inserted successfully into Neo4j.")
+    driver.close()
---
+++ kg_builder/management/commands/create_kg_relations.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+import logging
+from neo4j import GraphDatabase
+from ...helpers import create_kg_relations_helper as kgh
+from ...helpers import extract_text_helper as eth
+
+logger = logging.getLogger(__name__)
+
+uri = "bolt://localhost:7687"
+user = "neo4j"
+password = "neo4j1234!"
+database_name = "neo4j"
+
+
+class Command(BaseCommand):
+    help = 'This is a utility management command for creating knowledge graph relations and inserting it into database'
+
+    def handle(self, *args, **options):
+        graph_db = GraphDatabase.driver(uri, auth=(user, password))
+        kgh.create_and_insert_relations_into_kg("temp/sample.json")
+        graph_db.close()
